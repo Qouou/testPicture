c52c92ad6bade0e36966234ee124fc5e83fecae8
sister <lucyou871029@gmail.com>
2020-11-07 15:29:55 +0800

    init

---
+++ imageUpload/user_profilo/upload_profile/views.py
@@ -0,0 +1,22 @@
+from django.shortcuts import render
+from .models import User # 新增的程式碼
+
+# Create your views here.
+
+def detail(request):
+    list_user = User.objects.all() 
+    return render(request, 'upload_profile/detail.html', locals())
+
+
+def add(request):
+    # =====新增的程式碼=====#
+    if request.method == "POST":
+        user_name = request.POST.get('user_name')  # 對應剛剛add.html 中的input name
+        user_img = request.FILES.get('user_image')
+        introduce = request.FILES.get('introduce')
+        user = User(user_name=user_name, user_image=user_img, introduce=introduce)
+        user.save()
+        return render(request, 'upload_profile/add.html', locals())
+
+    # =====新增的程式碼=====#
+    return render(request, 'upload_profile/add.html', locals())
